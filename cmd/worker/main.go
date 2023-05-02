332be5705ea4e58cbf380619c2bf4ee09e54cf37
Jacob Lee <jacob.juhyung.lee@gmail.com>
2023-05-02 19:29:04 +0900

    Implement workflow starter and worker

---
+++ cmd/worker/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+
+	"github.com/jacobjlee/temporal-detection-workflow/workflow"
+)
+
+func main() {
+	// Create the client object just once per process
+	c, err := client.Dial(client.Options{})
+	if err != nil {
+		log.Fatalln("unable to create Temporal client", err)
+	}
+	defer c.Close()
+
+	// This worker hosts both Workflow and Activity functions
+	w := worker.New(c, workflow.GreetingTaskQueue, worker.Options{})
+	w.RegisterWorkflow(workflow.GreetingWorkflow)
+	w.RegisterActivity(workflow.ComposeGreeting)
+
+	// Start listening to the Task Queue
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("unable to start Worker", err)
+	}
+}
